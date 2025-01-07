64d5dcc0e86a20710a12bd11d43dbc3ce9c5b7f3
sneha05567 <2023aa05567@wilp.bits-pilani.ac.in>
2025-01-07 23:41:03 +0530

    Lint test_train.py
    Signed-off-by: sneha05567 <2023aa05567@wilp.bits-pilani.ac.in>

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,15 +1,20 @@
 import unittest  # For unit testing
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # Add parent directory to path
-
 from src.train import accuracy_score, load_iris, RandomForestClassifier, train_test_split
 
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+)
+
 class TestModel(unittest.TestCase):
+    
     def test_accuracy(self):
         # Load data and split into train/test sets
         data = load_iris()
-        X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(
+            data.data, data.target, test_size=0.2, random_state=42
+        )
         
         # Train Random Forest model and make predictions
         model = RandomForestClassifier()
@@ -22,4 +27,3 @@ class TestModel(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
