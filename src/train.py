2b81fd8568a5878d6ee2b4313fafbd43ecf389ac
sneha05567 <2023aa05567@wilp.bits-pilani.ac.in>
2025-01-08 00:08:22 +0530

    Lint train.py
    Signed-off-by: sneha05567 <2023aa05567@wilp.bits-pilani.ac.in>

--- src/train.py
+++ src/train.py
@@ -5,7 +5,8 @@ from sklearn.metrics import accuracy_score
 
 # Load dataset
 data = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+  data.data, data.target, test_size=0.2, random_state=42)
 
 # Train model
 model = RandomForestClassifier()
